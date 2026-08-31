a6a2eeed8d04c8342f4dcf4d7d1529d419168028
Nalysius <anthony.bocci@protonmail.com>
2026-08-31 20:38:46 +0200

    Use MaybeUninit instead of Vec<u8> for the buffer
    Better for alignment.

--- examples/sysctl_list_sensor_devices.rs
+++ examples/sysctl_list_sensor_devices.rs
@@ -5,7 +5,7 @@
 
 use i3status_again::sensors::sysctl::openbsd::headers::sensors::{SensorDev};
 use i3status_again::sensors::sysctl::openbsd::headers::sysctl::{CTL_HW, HW_SENSORS};
-
+use std::mem::MaybeUninit;
 
 use libc::{c_void, size_t, sysctl};
 
@@ -20,9 +20,10 @@ fn main() {
 		let mut size = size_of::<SensorDev>();
 
 		// Read the data of the sensor device #device_id
-		let mut buf: Vec<u8> = Vec::with_capacity(size);
+		let mut buf = MaybeUninit::uninit();
+		let ret: i32;
 		unsafe {
-			let ret = sysctl(
+			ret = sysctl(
 				mib.as_ptr(), // name
 				mib.len() as u32, // namelen
 				buf.as_mut_ptr() as *mut c_void, // oldp
@@ -30,23 +31,20 @@ fn main() {
 				std::ptr::null_mut(), // newp
 				0 as size_t // newlen
 			);
-			if ret == -1 {
-				println!("No more devices.");
-				break;
-			}
-			if size != size_of::<SensorDev>() {
-				println!("Size is invalid. A field could have been updated / added in SensorDev.");
-				break;
-			}
-			buf.set_len(size);
+		}
+		if ret == -1 {
+			println!("No more devices.");
+			break;
 		}
 
-		unsafe {
-			// Cast it to a SensorDev struct
-			let device: SensorDev = std::ptr::read(buf.as_ptr() as *const SensorDev);
-			let device_name = String::from_utf8(device.xname.to_vec()).unwrap();
-			println!("#{}: hw.sensors.{}", device.num, device_name);
+		if size != size_of::<SensorDev>() {
+			println!("Size is invalid. A field could have been updated / added in SensorDev.");
+			break;
 		}
+
+		let device: SensorDev = unsafe { buf.assume_init() };
+		let device_name = String::from_utf8(device.xname.to_vec()).unwrap();
+		println!("#{}: hw.sensors.{}", device.num, device_name);
 		device_id += 1;
 	}
 }
--- examples/sysctl_list_sensors.rs
+++ examples/sysctl_list_sensors.rs
@@ -6,6 +6,7 @@
 use i3status_again::sensors::sysctl::openbsd::headers::sensors::{SENSOR_MAX_TYPES, Sensor, SensorDev};
 use i3status_again::sensors::sysctl::openbsd::headers::sysctl::{CTL_HW, HW_SENSORS};
 use libc::{c_void, size_t, sysctl};
+use std::mem::MaybeUninit;
 
 fn main() {
 	// Loop over all the sensor devices
@@ -18,9 +19,10 @@ fn main() {
 		let mut size = size_of::<SensorDev>();
 
 		// Read the data of the sensor device #device_id
-		let mut buf: Vec<u8> = Vec::with_capacity(size);
+		let mut buf = MaybeUninit::uninit();
+		let ret: i32;
 		unsafe {
-			let ret = sysctl(
+			ret = sysctl(
 				mib.as_ptr(), // name
 				mib.len() as u32, // namelen
 				buf.as_mut_ptr() as *mut c_void, // oldp
@@ -28,27 +30,19 @@ fn main() {
 				std::ptr::null_mut(), // newp
 				0 as size_t // newlen
 			);
-			if ret != 0 {
-				println!("No more devices.");
-				break;
-			}
+		}
+		if ret != 0 {
+			println!("No more devices.");
+			break;
 		}
 
 		if size != size_of::<SensorDev>() {
 			println!("Size is invalid. A field could have been updated / added in SensorDev.");
 			break;
 		}
-		// Force buf len otherwise it's zero and casting to SensorDev is useless
-		unsafe {
-			buf.set_len(size);
-		}
 
-		let device: SensorDev;
+		let device: SensorDev = unsafe { buf.assume_init() };
 		let device_name: String;
-		unsafe {
-			// Cast buf to a SensorDev struct
-			device = std::ptr::read(buf.as_ptr() as *const SensorDev);
-		}
 		device_name = String::from_utf8(device.xname.to_vec()).unwrap();
 		
 		// Loop over the device' sensors
@@ -60,7 +54,7 @@ fn main() {
 				let mib = [CTL_HW, HW_SENSORS, device_id, sensor_type_id.try_into().unwrap(), sensor_id];
 				// The size is fixed, no need to call sysctl twice to get the size
 				let mut size = size_of::<Sensor>();
-				let mut buf: Vec<u8> = Vec::with_capacity(size);
+				let mut buf = MaybeUninit::uninit();
 				unsafe {
 					let ret = sysctl(
 						mib.as_ptr(), // name
@@ -79,15 +73,7 @@ fn main() {
 					println!("Size is invalid. A field could have been updated / added in Sensor.");
 					break;
 				}
-				// Force the length of buf
-				unsafe {
-					buf.set_len(size);
-				}
-				let sensor: Sensor;
-				unsafe {
-					// Cast it to a Sensor struct
-					sensor = std::ptr::read(buf.as_ptr() as *const Sensor);
-				}
+				let sensor: Sensor = unsafe { buf.assume_init() };
 
 				// Note: the values are raw. Example: hw.sensors.cpu0.temp is in
 				// micro Kelvin, not Celsius.

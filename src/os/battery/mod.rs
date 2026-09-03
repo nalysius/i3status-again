500ee5cb475a94c6a652b76763d363bf87a19bef
Nalysius <anthony.bocci@protonmail.com>
2026-09-03 20:42:40 +0200

    Remove a module documentation line
    Now that the architecture is clearly explained in docs/architecture.md,
    there is no need to explain that all os::battery submodules will need
    to reimplement the functions for the right OS.

--- src/os/battery/mod.rs
+++ src/os/battery/mod.rs
@@ -1,7 +1,5 @@
 //! The os::battery module impements an abstraction layer to get information
 //! about the battery.
-//! All supported OS must implement a submodule, and provide the required
-//! functions. See the public functions in the openbsd submodule for reference.
 
 use crate::sensors::sysctl::openbsd::SysctlError;
 use std::convert::From;
